950c4675c8a3c4ab116a82547e18b5c984b47c50
Justice M <codenamesolutions@gmail.com>
2020-05-05 00:25:05 +0200

    First commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+
+// Collapse and close navigation in mobile phones
+menuBtn.addEventListener("click", () =>{
+    nav.classList.toggle("menu-open");
+})
---
+++ assets/js/vendor.js
@@ -0,0 +1,2 @@
+let menuBtn = document.querySelector(".menu-btn");
+let nav = document.querySelector(".nav-links");
